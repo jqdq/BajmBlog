05700c30ffbac577db7a9db500957b347110fc10
PogromcaPapai <49349175+PogromcaPapai@users.noreply.github.com>
2019-05-19 20:23:34 +0200

    Uploaded a file with database functions

---
+++ cytaty.py
@@ -0,0 +1,18 @@
+import quicksqlite
+
+def poczatek_pracy():
+    ####
+    # Tworzy bazę danych 
+    ####
+    baza = quicksqlite.Connection()
+    baza.create_table(cytaty, ['nrop','utwor','cytat'], ["INTEGER","TEXT","TEXT"])
+
+def scrapuj():
+    ####
+    # Scrapuje teksty utworów
+    ####
+
+def pobierz():
+    ####
+    # Zwraca listę cytatów
+    ####
