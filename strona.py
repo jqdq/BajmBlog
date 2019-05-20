1c37d9c73735a888dd0fa65455bb0e063c0417a5
Jakub Dakowski <jakubdakowski@gmail.com>
2019-05-20 15:43:52 +0200

    Added a function to check time since the start of the program

--- strona.py
+++ strona.py
@@ -11,4 +11,5 @@ aplikacja = Flask(__name__)
 
 @aplikacja.route('/')
 def strona_glowna():
+    kiedy = round((clock() - start)/60,0)
     return render_template('test.html') # zamiast test.html trzeba potem wstawić 
