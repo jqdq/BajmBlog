d931d6c67cffe5be28fb20c43273ea0a88030116
PogromcaPapai <49349175+PogromcaPapai@users.noreply.github.com>
2019-05-24 14:58:37 +0200

    Moved the scraper into a function

--- cytaty.py
+++ cytaty.py
@@ -11,26 +11,6 @@ def poczatek_pracy():
     baza.create_table('main', ['nr','utwor','cytat'], ["INTEGER","TEXT","TEXT"])
     print('Baza utworzona')
     return baza
-'''
-def scrapuj(baza):
-    ####
-    # Scrapuje teksty utworów i dodaje je do bazy
-    ####
-    zbior=[]
-    nr+=1
-    baza.insert('main', [nr, utwor, cytat])
-    zbior.append(nr)
-    
-    shuffle(zbior)
-
-
-
-def pobierz():
-    ####
-    # Zwraca listę cytatów
-    ####
-'''
-
 
 def usuwajpuste(lista):
     nowa=[]
@@ -39,13 +19,23 @@ def usuwajpuste(lista):
             nowa.append(i)
     return nowa
 
-#tu jest wyszukiwanie linków ze stronki pozdrawiam
-
-stronka= requests.get('https://teksciory.interia.pl/bajm,a,1796.html')
-drzewunio=BeautifulSoup(strona.content, 'lxml')
-listazaznaczen= drzewunio.findAll('div', class_='artistContent marginRight20')[1].findAll('a',class_='title')
-listalinkow=dict()
-for i in listazaznaczen:
+def scrapuj(baza):
+    ####
+    # Scrapuje teksty utworów i dodaje je do bazy
+    ####
+    
+    # tu jest wyszukiwanie linków ze stronki pozdrawiam #
+    stronka= requests.get('https://teksciory.interia.pl/bajm,a,1796.html')
+    drzewunio=BeautifulSoup(strona.content, 'lxml')
+    listazaznaczen= drzewunio.findAll('div', class_='artistContent marginRight20')[1].findAll('a',class_='title')
+    listalinkow=dict()
+    for i in listazaznaczen:
     listalinkow[i.string]='https://teksciory.interia.pl'+i['href']
     print("Zakończono zbieranie linków")
+
+def pobierz():
+    ####
+    # Zwraca listę cytatów
+    ####
+
 
