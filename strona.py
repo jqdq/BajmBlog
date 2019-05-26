0fc239c0c7f96581bd568954dca43d23de717b68
ziebisty <50321799+ziebisty@users.noreply.github.com>
2019-05-26 11:17:56 +0200

    właściwa nazwa templejta ze stroną

--- strona.py
+++ strona.py
@@ -26,4 +26,4 @@ start = clock()
 @aplikacja.route('/')
 def strona_glowna():
     
-    return render_template('test.html', zmienne=pobierz(kursor, start)) # zamiast test.html trzeba potem wstawić 
+    return render_template(main_page.html, zmienne=pobierz(kursor, start))
